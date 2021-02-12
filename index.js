7560841e43894e9cbad6574104715f88fd25bd81
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2021-02-12 09:52:49 -0500

    Various updates
    added regex for email vaildation. package.json and added the node modules to .gitignore

---
+++ index.js
@@ -0,0 +1,45 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+const idArray = [];
+const teamArray = [];
+
+const indexMenu = () => {
+
+    const teamManager = () => {
+        inquirer.prompt([{
+                type: 'input',
+                name: 'managerName',
+                message: "What is your manager's name?"
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: "What is your manager's id?"
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is your manager's email address?",
+                validate: function (email) {
+                    // Regex mail check (return true if valid mail)
+                    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is your manager's office number?"
+            },
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerNumber)
+            teamArray.push(manager)
+        })
+    }
+
+
+
+}
+
+indexMenu();
